39f0726251bcef9eb4cfcdbe0b18aab643f80442
Akhilesh Yadav <yadavakhi1508@gmail.com>
2025-08-26 14:39:26 +0530

    customapi response and error

---
+++ src/utils/ApiError.js
@@ -0,0 +1,30 @@
+import { Error } from "mongoose";
+
+class ApiError extends Error{
+    constructor(
+        statusCode,
+        message = "Something went wrong",
+        errors=[],
+        stack =""
+        
+    ){
+        super(meessage)
+        this.statusCode= statusCode
+        this.data = null
+        this.message =message
+        this.success = false;
+        this.errors = errors
+
+        if (stack){
+            this.stack =stack
+
+        }
+        else{
+            Error.captureStackTrace(this,this.
+                constructor)
+        }
+    }
+
+}
+
+export {ApiError}
